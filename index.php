e6aae1219b1bc4c9afcd7ea29af11972847b9978
subbamaggus <markus.a.kerle@gmail.com>
2025-05-25 10:40:58 +0200

    add support for multiple projects

--- index.php
+++ index.php
@@ -2,11 +2,35 @@
 
 require('manager_req.php');
 
-$myManager = new RequirementManager();
+$active_project = "";
+if(isset($_GET["project"]))
+{
+    setcookie("active_project", $_GET["project"], time() + (86400 * 30), "/");
+}
 
-$dir = "./MyProject/requirements";
+if(isset($_COOKIE["active_project"]))
+{
+    $active_project = $_COOKIE["active_project"];
+}
 
-
+$projects_root = "./";
+$project_dirs = scandir($projects_root);
+$projects = array();
+$nav = "";
+foreach ($project_dirs as $key => $value)
+{
+    if (!in_array($value,array(".","..",".git")))
+    {
+        $file = $projects_root . DIRECTORY_SEPARATOR . $value;
+        if (is_dir($file))
+        {
+            $projects[] = $value;
+            $nav .= <<<END
+            <a href="?project={$value}">{$value}</a> - 
+            END;            
+        }
+    }
+}
 
 ?>
 <!doctype html>
@@ -45,61 +69,69 @@ $dir = "./MyProject/requirements";
     </style>
 </head>
 <body>
+<?php echo $nav; ?>
     <input type="button" value="store in vcs" />
     <input type="button" value="create documents" />
+<?php
+
+
+if("" <> $active_project)
+{
+?>
     <h1>requirements</h1>
     <input id="max" type="button" value="expand all" onclick="expand_all();" />
     <input id="min" type="button" value="minimize_all" onclick="minimize_all();" />
 <?php
-
-$files1 = scandir($dir);
-foreach ($files1 as $key => $value)
-{
-    if (!in_array($value,array(".","..")))
+    $myManager = new RequirementManager();
+    $dir = $projects_root . DIRECTORY_SEPARATOR . $active_project . DIRECTORY_SEPARATOR . "requirements";
+    $files1 = scandir($dir);
+    foreach ($files1 as $key => $value)
     {
-        $file = $dir . DIRECTORY_SEPARATOR . $value;
-        if (!is_dir($file))
+        if (!in_array($value,array(".","..")))
         {
-            $myReq = $myManager->getReqFromFile($file);
-
-            $element = <<<END
-                <hr class="new5">
-            <details>
-                <summary>{$myReq->Name}</summary>
-                <form action="save.php" method="POST">
-                    <input type="hidden" name="id" value="{$file}">
-                    <input type="hidden" name="Name" value="{$myReq->Name}"></label>
-                    <label>Topic<input name="Topic" value="{$myReq->Topic}"></label><br>
-                    <label>Type<input name="Type" value="{$myReq->Type}"></label><br>
-
-                    <label>Invented_on<input type="date" id="Invented_on" name="Invented_on" value="{$myReq->Invented_on}"></label><br>
-                    <label>Invented_by<input name="Invented_by" value="{$myReq->Invented_by}"></label><br>
-
-                    <label>Owner<input name="Owner" value="{$myReq->Owner}"></label><br>
-                    
-                    <br>
-
-                    <label>Status<input name="Status" value="{$myReq->Status}"></label><br>
-                    <label>Solved by<input name="Solved_by" value="{$myReq->Solved_by}"></label><br>
-                    <label>Priority<input name="Priority" value="{$myReq->Priority}"></label><br>
-                    <label>Effort_estimation<input name="Effort_estimation" value="{$myReq->Effort_estimation}"></label><br>
-
-                    <label>Description<textarea name="Description" cols="50" rows="4">{$myReq->Description}</textarea></label><br>
-                    <label>Rationale<textarea name="Rationale" cols="50" rows="4">{$myReq->Rationale}</textarea></label><br>
-
-                    <label>Test_Cases<input name="Test_Cases" value="{$myReq->Test_Cases}"></label>
-                    
-                    <P align="right"><input type="submit" value="save"></p>
-                </form>
-            </details>
-         \n
-         END;
-         
-            echo $element;
+            $file = $dir . DIRECTORY_SEPARATOR . $value;
+            if (!is_dir($file))
+            {
+                $myReq = $myManager->getReqFromFile($file);
+    
+                $element = <<<END
+                    <hr class="new5">
+                <details>
+                    <summary>{$myReq->Name}</summary>
+                    <form action="save.php" method="POST">
+                        <input type="hidden" name="id" value="{$file}">
+                        <input type="hidden" name="Name" value="{$myReq->Name}"></label>
+                        <label>Topic<input name="Topic" value="{$myReq->Topic}"></label><br>
+                        <label>Type<input name="Type" value="{$myReq->Type}"></label><br>
+    
+                        <label>Invented_on<input type="date" id="Invented_on" name="Invented_on" value="{$myReq->Invented_on}"></label><br>
+                        <label>Invented_by<input name="Invented_by" value="{$myReq->Invented_by}"></label><br>
+    
+                        <label>Owner<input name="Owner" value="{$myReq->Owner}"></label><br>
+                        
+                        <br>
+    
+                        <label>Status<input name="Status" value="{$myReq->Status}"></label><br>
+                        <label>Solved by<input name="Solved_by" value="{$myReq->Solved_by}"></label><br>
+                        <label>Priority<input name="Priority" value="{$myReq->Priority}"></label><br>
+                        <label>Effort_estimation<input name="Effort_estimation" value="{$myReq->Effort_estimation}"></label><br>
+    
+                        <label>Description<textarea name="Description" cols="50" rows="4">{$myReq->Description}</textarea></label><br>
+                        <label>Rationale<textarea name="Rationale" cols="50" rows="4">{$myReq->Rationale}</textarea></label><br>
+    
+                        <label>Test_Cases<input name="Test_Cases" value="{$myReq->Test_Cases}"></label>
+                        
+                        <P align="right"><input type="submit" value="save"></p>
+                    </form>
+                </details>
+            \n
+            END;
+            
+                echo $element;
+            }
         }
     }
 }
-
 ?>
 </body>
 </html>
